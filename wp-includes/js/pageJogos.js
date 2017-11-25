70a079b66b91b0350daf3a863737539481cee93a
José Luiz do Carmo Silva <joseluiz98.jl@gmail.com>
2017-11-24 22:22:08 -0200

    Slider Resultados criado
    - Slider da página "Jogos e Resultados" em JQuery criado

--- wp-includes/js/pageJogos.js
+++ wp-includes/js/pageJogos.js
@@ -2,53 +2,81 @@ $(document).ready(function(){
 	//Esconde todos os slides
 	$(".content > div").hide();
 	
-	//Pega o elemento inicial e em seguida o exibe
-	var elementoAtivo = $('[id*="active"]');
-	elementoAtivo.show();
-	
+	//Configura o estilo dos hovers (verde com o cursor em cima, verifica a cor quando estiver sem o cursor)
+	$('.carousel-buttons').on('mouseenter', '.glyphicon-chevron-left', function () {
+    	$(this).css("color", "green");
+	}).on('mouseleave', '.glyphicon-chevron-left', function () {
+    	atribuiCoresBotoes();
+	});
+	//Este é do botão direito
+	$('.carousel-buttons').on('mouseenter', '.glyphicon-chevron-right', function () {
+    	$(this).css("color", "green");
+	}).on('mouseleave', '.glyphicon-chevron-right', function () {
+    	atribuiCoresBotoes();
+	});
+
+	//Pega o slide inicial e o exibe
+	var slideAtivo = $('[id*="active"]');
+	atribuiCoresBotoes();
+	slideAtivo.show();
+
 	//Recupera e adicionar título inicial do slide
-	var tituloAtivo = elementoAtivo.children().find('h2').text();
-	elementoAtivo.children().find('h2').hide();
+	var tituloAtivo = slideAtivo.children().find('h2').text();
+	slideAtivo.children().find('h2').hide();
 	$("#chave-titulo h3").html(tituloAtivo);
 
-	var totalElementos = $(".content > div").length;
+	var totalslides = $(".content > div").length;
 
+	//User clicou para mudar de slide, trate
 	$(".carousel-buttons > div > h2 > a").click(function(event){
-		var indexAtual = elementoAtivo.index();
+		var indexAtual = slideAtivo.index();
 		//User clicou em anterior
 		if($(this).attr("id") == "prev"){
-			var indexProximoElemento = elementoAtivo.index() - 1;
-			if (indexProximoElemento >= 0){
-				var proximoElemento = $(".content > div").eq(indexProximoElemento); //Salva elemento cujo index é o mesmo presente em .content
-				tituloAtivo = proximoElemento.children().find('h2').text();
-				proximoElemento.children().find('h2').hide();
+			var indexProximoSlide = slideAtivo.index() - 1;
+			if (indexProximoSlide >= 0){
+				var proximoSlide = $(".content > div").eq(indexProximoSlide); //Salva slide cujo index é o mesmo presente em .content
+				tituloAtivo = proximoSlide.children().find('h2').text();
+				proximoSlide.children().find('h2').hide();
 				$("#chave-titulo h3").html(tituloAtivo);
-				elementoAtivo.hide();
-				elementoAtivo = proximoElemento;
-				proximoElemento.show(400);
+				slideAtivo.hide();
+				slideAtivo = proximoSlide;
+				proximoSlide.show();
 			}
 		}
 		//User clicou em próximo
 		else {
-			var indexProximoElemento = elementoAtivo.index() + 1;
-			if (indexProximoElemento < totalElementos){
-				var proximoElemento = $(".content > div").eq(indexProximoElemento); //Salva elemento cujo index é o mesmo presente em .content
-				tituloAtivo = proximoElemento.children().find('h2').text();
-				proximoElemento.children().find('h2').hide();
+			var indexProximoSlide = slideAtivo.index() + 1;
+			if (indexProximoSlide < totalslides){
+				var proximoSlide = $(".content > div").eq(indexProximoSlide); //Salva slide cujo index é o mesmo presente em .content
+				tituloAtivo = proximoSlide.children().find('h2').text();
+				proximoSlide.children().find('h2').hide();
 				$("#chave-titulo h3").html(tituloAtivo);
-				elementoAtivo.hide();
-				elementoAtivo = proximoElemento;
-				elementoAtivo.show(400);
+				slideAtivo.hide();
+				slideAtivo = proximoSlide;
+				slideAtivo.show();
 			}
 		}
-		
-		/*if(elementoFilho.css("visibility") == "visible"){
-			alert("caio!");
-			elementoFilho.hide("slow");
-		} else{
-			alert("não caio!");
-			alert(elementoFilho.attr("id"));
-			elementoFilho.show("slow");
-		}*/
+		atribuiCoresBotoes();
 	 });
+
+	///Esta função testa quais cores os botões devem receber
+	///Black = existe um slide após o atual
+	///Lightgrey = este é o´último slide nesta direção
+	function atribuiCoresBotoes() {
+		//Se é o primeiro slide à esquerda, colore lightgrey
+		if(slideAtivo.index() == 0) {
+			$(".glyphicon-chevron-left").css("color", "lightgrey");
+			$(".glyphicon-chevron-right").css("color", "black");
+		}
+		//Se é o último slide à direita, colore lightgrey
+		else if(slideAtivo.index() == totalslides-1){
+			$(".glyphicon-chevron-left").css("color", "black");
+			$(".glyphicon-chevron-right").css("color", "lightgrey");
+		}
+		//Senão, os dois recebem black
+		else {
+			$(".glyphicon-chevron-left").css("color", "black");
+			$(".glyphicon-chevron-right").css("color", "black");
+		}
+	}
 })
