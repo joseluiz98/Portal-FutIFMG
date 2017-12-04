6619cc8e00d12950e91802b629967b17b8a82b8b
José Luiz do Carmo Silva <joseluiz98.jl@gmail.com>
2017-12-04 18:59:13 -0200

    Revert "Merge branch 'master' of https://github.com/joseluiz98/Portal-FutIFMG"
    This reverts commit 2739dcb0506f000d2dc752ffcf2c756373b8714c, reversing
    changes made to 3aee828ce53d432b7940a824a280370ca8f39db3.

--- wp-includes/php/classificacao.php
+++ wp-includes/php/classificacao.php
@@ -119,19 +119,11 @@ function preencheTabela($conn,$grupo)
                   		echo '<span class="game-content">';
 	                  	echo '<span class="nomeMandante">'.$rowMandante["nomeTime"].'</span>';
 <<<<<<< HEAD
-<<<<<<< HEAD
                       	echo '<img class="escudoTime escudoMandante" src='.$rowMandante["escudo"].'></img>';
                   		echo " " . $rowMandante["placarCasa"];
                 	  	echo ' x ';
 	                  	echo $rowVisitante["placarVisitante"]. " ";
                       	echo '<img class="escudoTime escudoVisitante" src='.$rowVisitante["escudo"].'></img>';
-=======
-                      		echo '<img class="escudoTime escudoManda" src='.$rowMandante["escudo"].'></img>';
-                  		echo " " . $rowMandante["placarCasa"];
-                	  	echo ' x ';
-	                  	echo $rowVisitante["placarVisitante"]. " ";
-                      		echo '<img class="escudoTime escudoVisitante" src='.$rowVisitante["escudo"].'></img>';
->>>>>>> 8dbf4b940bb318b8756c438c038ae1c9fe6ce540
 =======
                       		echo '<img class="escudoTime escudoManda" src='.$rowMandante["escudo"].'></img>';
                   		echo " " . $rowMandante["placarCasa"];
