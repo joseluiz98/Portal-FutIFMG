8399d27c92063f925712e6832bcc1e40c289ce73
José Luiz do Carmo Silva <joseluiz98.jl@gmail.com>
2018-06-06 22:44:34 -0300

    query proximo-jogo widget fixed, homepage image size fixed

--- wp-includes/mysql/queries/recuperaIDsProximoJogo.sql
+++ wp-includes/mysql/queries/recuperaIDsProximoJogo.sql
@@ -1,4 +1,4 @@
-SELECT data AS dataJogo, fk_idTime AS idTimeCasa, fk_idVisitante AS idTimeVisitante
-FROM jogo INNER JOIN jogo_tem_times 
-ON jogo.idJogo = jogo_tem_times.fk_idJogo 
-AND (jogo.data = (SELECT MAX(data) FROM jogo))
+SELECT J.data AS dataJogo, JT.fk_idTime AS idTimeCasa, JT.idVisitante AS idTimeVisitante
+FROM jogo J INNER JOIN jogo_tem_times JT
+ON J.idJogo = JT.fk_idJogo 
+AND (J.data = (SELECT MAX(data) FROM jogo));
